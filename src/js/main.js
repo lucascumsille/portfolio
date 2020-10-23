ff0248acf01b976373d904612039ebf82bc9815f
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-10-23 17:31:20 +1100

    Vendor included and icons now can change color upon variable

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,16 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     nav:true,
     dots: false,
+    autoWidth:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:3
+            items:1
         }
     }
 })
