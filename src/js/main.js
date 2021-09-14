86f0f7186b49e27a1a52c2a0010a4acda3ebb7d1
Lucas Cumsille M <lucas.cumsille@gmail.com>
2021-09-14 16:58:17 +0100

    fonts fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,30 @@
-$('.owl-carousel').owlCarousel({
+$('.about-carousel').owlCarousel({
     loop:true,
     nav:false,
     dots: false,
     autoplay:true,
     autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+
+})
+
+$('#testimonial-carousel').owlCarousel({
+    //loop:true,
+    nav:false,
+    dots: false,
+    //autoplay:true,
+    //autoplayTimeout:5000,
     autoplayHoverPause:true,
     responsive:{
         0:{
